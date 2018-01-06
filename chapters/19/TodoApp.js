dfb7bec56700fd7ac0a77c9021409fc437eeadc1
Girma Nigusse <xgirma@gmail.com>
2018-01-06 08:38:20 -0800

    ch.19

--- chapters/19/TodoApp.js
+++ chapters/19/TodoApp.js
@@ -51,10 +51,10 @@ class TodoApp extends React.Component {
         </ul>
 
         <p>
-          Show:{' '}
-          <FilterLink filter='SHOW_ALL' currentFilter={visibilityFilter}> All </FilterLink>{'  '}
-          <FilterLink filter='SHOW_ACTIVE' currentFilter={visibilityFilter}> Active </FilterLink>{'  '}
-          <FilterLink filter='SHOW_ACTIVE' currentFilter={visibilityFilter}> Completed </FilterLink>
+          Show:{' ['}
+          <FilterLink filter='SHOW_ALL' currentFilter={visibilityFilter}> All </FilterLink>{']  ['}
+          <FilterLink filter='SHOW_ACTIVE' currentFilter={visibilityFilter}> Active </FilterLink>{']  ['}
+          <FilterLink filter='SHOW_COMPLETED' currentFilter={visibilityFilter}> Completed </FilterLink>{']'}
         </p>
       </div>
     );
--- chapters/19/todoAppReducer.js
+++ chapters/19/todoAppReducer.js
@@ -38,7 +38,7 @@ const todos = (state = [], action) => {
 const visibilityFilter = (state = 'SHOW_ALL', action) => {
   switch (action.type) {
     case 'SET_VISIBILITY_FILTER':
-      return {filter: action.filter};
+      return action.filter;
     default:
       return state;
   }
